4a281c62d414f458d8a06754c6be19552f4da07d
Emil Grip <emilgrip@msn.com>
2018-02-27 15:25:51 +0100

    Test commit, add comments

--- App/System/FormValidator.php
+++ App/System/FormValidator.php
@@ -6,93 +6,96 @@ use \App\Models\UsersModel;
 
 class FormValidator {
 
-    private $errors = [];
+	private $errors = [];
 
-    // T0D0 - sanitize the whole form, especially string inputs and file upload
-    public function notEmpty($element, $value, $message) {
-        if(empty($value)) {
-            $this->errors[$element] = $message;
-            return false;
-        }
-        return true;
-    }
+	// T0D0 - sanitize the whole form, especially string inputs and file upload
+	public function notEmpty($element, $value, $message) {
+		if (empty($value)) {
+			$this->errors[$element] = $message;
+			return false;
+		}
+		return true;
+	}
 
-    public function validCategory($element, $value, $message) {
-        $model    = new CategoriesModel();
-        $category = $model->find($value);
-        if(!$category) {
-            $this->errors[$element] = $message;
-        }
-    }
+	public function validCategory($element, $value, $message) {
+		$model = new CategoriesModel();
+		$category = $model->find($value);
+		if (!$category) {
+			$this->errors[$element] = $message;
+		}
+	}
 
-    public function validPassword($element, $value, $value_verification, $message) {
-        if(empty($value) || ($value != $value_verification)) {
-            $this->errors[$element] = $message;
-        }
-    }
+	# Emil: For AUTH-007
+	public function validPassword($element, $value, $value_verification, $message) {
+		if (empty($value) || ($value != $value_verification)) {
+			$this->errors[$element] = $message;
+		}
+	}
 
-    public function validUsername($element, $value, $message) {
-        if(!preg_match('/[a-z0-9]+/', $value)) {
-            $this->errors[$element] = $message;
-        }
-    }
+	public function validUsername($element, $value, $message) {
+		if (!preg_match('/[a-z0-9]+/', $value)) {
+			$this->errors[$element] = $message;
+		}
+	}
 
-    public function availableUsername($element, $value, $message) {
-        $model  = new UsersModel();
-        $result = $model->query("SELECT * FROM users WHERE username = ?", [
-            $value
-        ], true);
+	public function availableUsername($element, $value, $message) {
+		$model = new UsersModel();
+		$result = $model->query("SELECT * FROM users WHERE username = ?", [
+			$value,
+		], true);
 
-        if($result) {
-            $this->errors[$element] = $message;
-        }
-    }
+		if ($result) {
+			$this->errors[$element] = $message;
+		}
+	}
 
-    public function validEmail($element, $value, $message) {
-        if (!empty($value)){
-            if(!filter_var($value, FILTER_VALIDATE_EMAIL)) {
-                $this->errors[$element] = $message;
-            }
-        }
-    }
-    
-    public function isNumeric($element, $value, $message) {
-        if(!is_numeric($value)) {
-            $this->errors[$element] = $message;
-        }
-    }
+	public function validEmail($element, $value, $message) {
+		if (!empty($value)) {
+			if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+				$this->errors[$element] = $message;
+			}
+		}
+	}
 
-    public function isInteger($element, $value, $message) {
-        if(!is_int(intval($value))) {
-            $this->errors[$element] = $message;
-        }
-    }
+	public function isNumeric($element, $value, $message) {
+		if (!is_numeric($value)) {
+			$this->errors[$element] = $message;
+		}
+	}
 
-    public function validImage($element, $value, $message) {
-        if(empty($value)) {
-            $this->errors[$element] = $message;
-        }
+	public function isInteger($element, $value, $message) {
+		if (!is_int(intval($value))) {
+			$this->errors[$element] = $message;
+		}
+	}
 
-        else {
-            if(empty($value['type'])) {
-                $this->errors[$element] = $message;
-                return;
-            }
+	public function validImage($element, $value, $message) {
+		if (empty($value)) {
+			$this->errors[$element] = $message;
+		} else {
+			if (empty($value['type'])) {
+				$this->errors[$element] = $message;
+				return;
+			}
 
-            if($value['size'] > 1000000) {
-                $this->errors[$element] = "Your media is too big (> 1Mo)";
-                return;
-            }
-        }
-    }
+			if ($value['size'] > 1000000) {
+				$this->errors[$element] = "Your media is too big (> 1Mo)";
+				return;
+			}
+		}
+	}
 
-    public function isValid() {
-        if(empty($this->errors)) return true;
-        else return false;
-    }
+	public function isValid() {
+		if (empty($this->errors)) {
+			return true;
+		} else {
+			return false;
+		}
 
-    public function getErrors() {
-        return $this->errors;
-    }
+	}
+
+	public function getErrors() {
+		return $this->errors;
+	}
 
 }
