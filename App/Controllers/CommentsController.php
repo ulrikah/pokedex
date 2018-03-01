75590f99696b8e98a6cae1689806b3c983fd1a89
olavskogen <olaskoge@stud.ntnu.no>
2018-03-01 13:41:49 +0100

    Filtrering av text input

--- App/Controllers/CommentsController.php
+++ App/Controllers/CommentsController.php
@@ -13,6 +13,11 @@ class CommentsController extends Controller {
     public function add() {
         if(!empty($_POST)){
                 $text  = isset($_POST['comment']) ? $_POST['comment'] : '';
+                
+                //Filtering av text input
+                //$text = mysql_real_escape_string($text);
+                $text = strip_tags($text);
+
                 $model = new CommentsModel;
                 $model->create([
                     'created_at' => date('Y-m-d H:i:s'),
